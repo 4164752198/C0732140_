278fb9c96c69db953ff35ee5c963ec76eff00ec1
visualstudio <visualstudio@230-PC41>
2019-04-04 17:19:58 -0400

    Web Application

--- C0732140_/Program.cs
+++ C0732140_/Program.cs
@@ -1,44 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace C0732140
-{
-    public delegate void Sort_DataStructure();
-
-    class Program
-    {
-        static void Main(string[] args) { }
-    }
-
-    class Student
-    {
-        String Student_name;
-        String StudentID;
-        double StudentGPA;
-
-        // TODO: change this implementation so that the program pulls names from a Text File:
-        // to do this, you must change the container for Student Names from Array to List
-        String[] names = new String[5] { "Bill", "Mary", "Laura", "Sam", "Steve" };
-        Random r1;
-
-        public Student()
-        {
-            r1 = new Random();
-            Student_name = names[r1.Next(0, 4)];
-            StudentID = Convert.ToString(r1.Next(1000, 9999));
-            StudentGPA = Convert.ToDouble(r1.Next(1, 99));
-        }
-
-        public double AverageStudentGPA()
-        {
-            // TO DO: implement the algorithm to calculate the Students' average GPA
-            return 0;
-        }
-    }
+﻿
 
     public interface IDataStructure
     {
